3cd0a69565effc02bbac73a6f2b08658e9266471
MrOtaiLy <lykovvana824@gmail.com>
2025-02-28 17:00:30 +0300

    Автоматический ответ

--- app.py
+++ app.py
@@ -29,6 +29,36 @@ def list_prof(list_type):
     ]
     return render_template('list_prof.html', list_type=list_type, professions=professions)
 
+@app.route('/answer', methods=['GET', 'POST'])
+def answer():
+    if request.method == 'GET':
+        return render_template('answer_form.html')
+    else:
+        data = {}
+        data['title'] = request.form['title']
+        data['surname'] = request.form['surname']
+        data['name'] = request.form['name']
+        data['education'] = request.form['education']
+        data['profession'] = request.form['profession']
+        data['sex'] = request.form['sex']
+        data['motivation'] = request.form['motivation']
+        data['ready'] = request.form['ready']
+        return render_template('auto_answer.html', data=data)
+
+@app.route('/auto_answer')
+def auto_answer():
+    data = {
+        "title": "Анкета",
+        "surname": "Watny",
+        "name": "Mark",
+        "education": "Высшее",
+        "profession": "штурман марсохода",
+        "sex": "male",
+        "motivation": "Всегда мечтал застрять на Марсе!",
+        "ready": "True"
+    }
+    return render_template('auto_answer.html', data=data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
