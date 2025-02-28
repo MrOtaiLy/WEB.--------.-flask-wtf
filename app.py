87a31a51fe61c784c555f61ac076a6885079bb8f
MrOtaiLy <lykovvana824@gmail.com>
2025-02-28 16:37:13 +0300

    Тренировки в полёте

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ def index():
     title = request.args.get('title', 'Заглушка')
     return render_template('base.html', title=title)
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
 if __name__ == '__main__':
     app.run(debug=True)
